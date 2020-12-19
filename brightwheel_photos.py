4a1bd2b0ddc9ca795ae4395a0d09ffab785f8910
Johnathan Gilday <me@johnathangilday.com>
2020-12-18 22:15:43 -0500

    :construction: Photo download working

--- brightwheel_photos.py
+++ brightwheel_photos.py
@@ -2,12 +2,15 @@
 
 import argparse
 import json
+from urllib.parse import urlparse
+
 import requests
 
 def main():
     parser = argparse.ArgumentParser(description='Download photos from Brightwheel')
     parser.add_argument('--email', help='')
     parser.add_argument('--password', help='')
+    parser.add_argument('--directory', help='')
     args = parser.parse_args()
 
     with requests.Session() as s:
@@ -34,10 +37,15 @@ def main():
                 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.16; rv:83.0) Gecko/20100101 Firefox/83.0'
         }
         r = s.get('https://schools.mybrightwheel.com/api/v1/students/4a1fe4c5-0a33-45c4-9e1c-a470fb28d3e2/activities', headers=headers, params=params)
-        print(r.url)
-        print(r.request.headers)
-        response = r.json()
-        print(response)
+        data = r.json()
+        for activity in data['activities']:
+            url = activity['media']['image_url']
+            path = urlparse(url).path.split('/')[-1]
+            with open('{directory}/{path}'.format(directory=args.directory, path=path), 'wb') as f:
+                r = s.get(url)
+                f.write(r.content)
+
+
 
 
 if __name__ == "__main__":
