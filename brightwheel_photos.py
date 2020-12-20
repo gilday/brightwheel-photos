826a5cb86dbbdbc26fc4a29a0886d811e5283820
Johnathan Gilday <me@johnathangilday.com>
2020-12-19 21:12:44 -0500

    :construction: Refactor large method

--- brightwheel_photos.py
+++ brightwheel_photos.py
@@ -22,72 +22,85 @@ def main():
 
     os.makedirs(args.directory, exist_ok=True)
     with requests.Session() as s:
-
-        # login
-        login_data = {
-            'user': {
-                'email': args.email,
-                'password': args.password
-            }
-        }
-        headers = {
-                'Accept': 'application/json',
-                'X-Client-Name': 'web',
-                'X-Client-Version': 'b15cec31e66fa803de35b53260872aa7e5e84e29',
-                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.16; rv:83.0) Gecko/20100101 Firefox/83.0'
-        }
-        r = s.post('https://schools.mybrightwheel.com/api/v1/sessions', headers=headers, json=login_data)
-        r.raise_for_status()
-        csrf = r.json()['csrf']
-
+        login(s, args.email, args.password)
         # try to find student_id if not provided
         student_id = args.student_id
         if not student_id:
-            r = s.get('https://schools.mybrightwheel.com/api/v1/users/me', headers=headers)
-            guardian_id = r.json()['object_id']
-
-            r = s.get('https://schools.mybrightwheel.com/api/v1/guardians/{}/students'.format(guardian_id))
-            data = r.json()
-            if data['count'] > 1:
+            students = find_students(s)
+            if len(students) > 1:
                 print('Multiple students detected: pick one', file=sys.stderr)
-                for student in data['students']:
-                    student = student['student']
+                for student in students:
                     print('{} {} {}'.format(student['object_id'],student['first_name'],student['last_name'], file=sys.stderr))
                 sys.exit(1)
                 return
-            student_id = data['students'][0]['student']['object_id']
+            student_id = students[0]['object_id']
 
 
         # find and save all photos for the student
-        params = {'page': 0, 'page_size': 10, 'action_type': 'ac_photo', 'include_parent_actions': 'true'}
-        headers = {
-                'Accept': 'application/json',
-                'X-CSRF-Token': csrf,
-                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.16; rv:83.0) Gecko/20100101 Firefox/83.0'
-        }
-        page = 0
-        page_size = 10
-        while (True):
-            params['page'] = page
-            params['offset'] = page * page_size
-            r = s.get('https://schools.mybrightwheel.com/api/v1/students/4a1fe4c5-0a33-45c4-9e1c-a470fb28d3e2/activities', headers=headers, params=params)
-            data = r.json()
-            activities = data['activities']
-            if len(activities) <= 0:
-                return
-            for activity in data['activities']:
-                url = activity['media']['image_url']
-                path = urlparse(url).path.split('/')[-1]
-                r = s.get(url)
-                image = PngImageFile(io.BytesIO(r.content))
-                metadata = PngInfo()
-                created_at = datetime.strptime(activity['created_at'], "%Y-%m-%dT%H:%M:%S.%f%z")
-                metadata.add_text('Creation Time', email.utils.format_datetime(created_at))
+        for activity in find_photo_activities(s):
+            print(json.dumps(activity))
+            url = activity['media']['image_url']
+            r = s.get(url)
+            image = PngImageFile(io.BytesIO(r.content))
+            metadata = PngInfo()
+            created_at = datetime.strptime(activity['created_at'], "%Y-%m-%dT%H:%M:%S.%f%z")
+            metadata.add_text('Creation Time', email.utils.format_datetime(created_at))
+            note = activity['note']
+            if note:
                 metadata.add_text('Description', activity['note'])
-                image.save('{directory}/{path}'.format(directory=args.directory, path=path), pnginfo=metadata)
-            page += 1
-            return
+            path = urlparse(url).path.split('/')[-1][:-3]
+            image.save('{directory}/{path}.jpg'.format(directory=args.directory, path=path), pnginfo=metadata)
 
+
+def login(s, email, password):
+    # login
+    login_data = {
+        'user': {
+            'email': email,
+            'password': password
+        }
+    }
+    headers = {
+        'X-Client-Name': 'web',
+        'X-Client-Version': 'b15cec31e66fa803de35b53260872aa7e5e84e29',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.16; rv:83.0) Gecko/20100101 Firefox/83.0'
+    }
+    r = s.post('https://schools.mybrightwheel.com/api/v1/sessions', headers=headers, json=login_data)
+    r.raise_for_status()
+    csrf_token = r.json()['csrf']
+    headers['X-CSRF-Token'] = csrf_token
+    s.headers.update(headers)
+
+
+
+def find_students(s):
+    headers = {
+        'Accept': 'application/json'
+    }
+    r = s.get('https://schools.mybrightwheel.com/api/v1/users/me', headers=headers)
+    r.raise_for_status()
+    guardian_id = r.json()['object_id']
+
+    r = s.get('https://schools.mybrightwheel.com/api/v1/guardians/{}/students'.format(guardian_id))
+    return [record['student'] for record in r.json()['students']]
+
+
+def find_photo_activities(s):
+    page = 0
+    page_size = 10
+    params = {'page_size': page_size, 'action_type': 'ac_photo', 'include_parent_actions': 'true'}
+    while (True):
+        params['page'] = page
+        params['offset'] = page * page_size
+        r = s.get('https://schools.mybrightwheel.com/api/v1/students/4a1fe4c5-0a33-45c4-9e1c-a470fb28d3e2/activities', params=params)
+        data = r.json()
+        activities = data['activities']
+        if len(activities) <= 0:
+            return
+        for activity in activities:
+            yield activity
+        page += 1
+ 
 
 if __name__ == "__main__":
     main()
